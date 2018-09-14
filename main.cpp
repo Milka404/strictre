5cffe706c7cb54a19b2b24fde04f2ac37f432822
milka404 <Milka2158@gmail.com>
2018-09-14 17:34:14 +0700

    The implementation of the adding element and the output

--- main.cpp
+++ main.cpp
@@ -4,16 +4,17 @@
 using namespace std;
 
 struct List {
-    double *a;
+    double a;
      List *next;
 };
- List* Read(List *L){
+ List* Read(List *L){       // читам список
      List *a = new List;
      a -> next = NULL;
      cout << "Введите элемент: "<< endl;
      cin >> a-> a;
 
      if (L == NULL){
+         
          L == a;
      }
      else{
@@ -24,7 +25,47 @@ struct List {
      return L;
  }
 
-void Delete(int p, List *&L)
+ void Printlist(List *L){   //Выводим
+     while (L !=NULL){
+         cout << L -> a << " ";
+         L = L -> next;
+     }
+     cout << endl;
+ }
+
+ List *End (List *L){   //Конец
+     while (L !=NULL){
+         L = L->next;
+     }
+     return L;
+ }
+
+ List *First(List *L){
+     return L;
+ }
+
+ void Add (List *&top, int pos, int x){
+     int j = 1;
+     int i ;
+     List *pnew, *p = top;
+     pnew = new List; pnew -> a = x;
+     if (pos <= 1 || top == NULL){
+         pnew -> next = top; top = pnew;
+     }
+     else {
+         for (i = 1; i< pos - 1; i++)
+             if (p -> next != NULL){
+                 p = p -> next;
+                 j++;
+             }
+             if (pos <= j + 1){
+                 pnew -> next = p -> next = pnew;
+             }
+     }
+ }
+
+
+void Delete(int p, List *&L)  //Удаление элемента
 {
     List*& a = L;
     List* b = a->next;
@@ -53,9 +94,11 @@ void Delete(int p, List *&L)
       int c;
       do{
           system("csl");
-          cout<<" Menu "<<endl<<" 1. Введите элемент"<<endl<<" 2. Удаление элемента"<<endl;
-          cout<<"Ведите команду: "<<endl;
-          cin>>c;
+          cout <<" Menu "<<endl<<" 1. Введите элемент"<< endl <<" 2. Удаление элемента"<< endl << "3. Сначало" << endl;
+          cout <<"4. Вывести список" << endl;
+          cout << "5. Добавить элемент" << endl;
+          cout <<"Ведите команду: "<<endl;
+          cin >> c;
           switch (c){
               case 1:
                   L = Read(L);
@@ -67,15 +110,32 @@ void Delete(int p, List *&L)
                   cin >> p;
                   Delete(p, L);
                   break;
+              case 3:
+                  if (First(L) != NULL)
+                    cout << First(L) -> a << endl;
+                  else
+                      cout << "Список пуст "<< endl;
+                  break;
+              case 4:
+                  Printlist(L);
+                  break;
+              case 5:
+                  int pos, x;
+                  cout << "Введите номер для добавления: ";
+                  cin >> pos;
+                  cout << "Введите элемент добавления: ";
+                  cin >> x;
+                  Add( L, pos, x);
           }
-
+          system("pause");
       }
+      while (c != 0);
  }
 
 int main() {
 
      List* list = NULL;
-     Menu (List);
+     Menu (list);
      system("pause");
 
     return 0;
